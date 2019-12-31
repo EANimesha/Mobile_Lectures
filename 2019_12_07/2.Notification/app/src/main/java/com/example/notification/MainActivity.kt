7bcc8c44e5fb3371cc3577f202af60cac8dd3cb6
EANimesha <nimeshadilini999@gmail.com>
2019-12-31 23:26:17 +0530

    send notifications using channels

---
+++ 2019_12_07/2.Notification/app/src/main/java/com/example/notification/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.notification
+
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+
+@SuppressLint("ByteOrderMark")
+class MainActivity : AppCompatActivity() {
+
+    private var notificationManager: NotificationManager? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        notificationManager =
+            getSystemService(
+                Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        createNotificationChannel(
+            "com.ebookfrenzy.notifydemo.news",
+            "NotifyDemo News",
+            "Example News Channel")
+
+        btn.setOnClickListener {
+            val notificationID = 101
+
+            val channelID = "com.ebookfrenzy.notifydemo.news"
+
+            val notification = Notification.Builder(this@MainActivity,channelID)
+                .setContentTitle("Example Notification")
+                .setContentText("This is an  example notification.")
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setChannelId(channelID)
+                .build()
+
+            notificationManager?.notify(notificationID, notification)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(id: String, name: String,
+                                          description: String) {
+
+        val importance = NotificationManager.IMPORTANCE_LOW
+        val channel = NotificationChannel(id,name,importance)
+
+        channel.description = description
+        channel.enableLights(true)
+        channel.lightColor = Color.RED
+        channel.enableVibration(true)
+        channel.vibrationPattern =
+            longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+        notificationManager?.createNotificationChannel(channel)
+    }
+
+}
